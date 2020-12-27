50f6d3f59127b4805a7f94871b0c970537696b63
fenirani <fenirani@live.com>
2020-12-27 05:31:17 +0000

    Add a google graph

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { Socket } from './Socket';
 import { Ranking } from './Ranking';
+import Chart from "react-google-charts";
 
 export function Content() {
 
-    const [word, setWord] = React.useState("Fuck");
+    const [word, setWord] = React.useState(0);
     
     function newZip() {
         React.useEffect(() => { 
@@ -18,8 +19,23 @@ export function Content() {
     
     return (
         <div>
-            {  word }
             <Ranking />
+            <Chart
+              width={'500px'}
+              height={'300px'}
+              chartType="PieChart"
+              loader={<div>Loading Chart</div>}
+              data={[
+                ['Game', 'Votes'],
+                ['Gloomhaven', word],
+                ['Dominant Species', 1],
+                ['Clank (Fantasy)', 3],
+                ['Inis', 6],
+                ['Terraforming Mars', 7],
+              ]}
+              options={{
+                title: '',}}
+            />
         </div>
     );
 }
