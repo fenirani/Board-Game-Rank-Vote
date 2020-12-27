08c7713febfd1b3d382795fcdddd14454c18cea1
fenirani <fenirani@live.com>
2020-12-27 05:08:41 +0000

    Finish adding pictures

--- scripts/Ranking.jsx
+++ scripts/Ranking.jsx
@@ -49,7 +49,7 @@ export function Ranking() {
                 Gloomhaven
                 <img
                 src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "gloomhaven">
                 	<option> 5 </option>
@@ -62,7 +62,7 @@ export function Ranking() {
                 Terraforming Mars
                 <img 
                 src= "https://images-na.ssl-images-amazon.com/images/I/91RdR7olLsL._AC_SL1500_.jpg"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "tm">
                 	<option> 5 </option>
@@ -75,7 +75,7 @@ export function Ranking() {
                 Battle For Rokugan
                 <img 
                 src= "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSiechUaQT4nPMTTcTQC9EEUN_LA-rxT4ZQdkQchVwZ3iQ3_v0"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "roku">
                 	<option> 5 </option>
@@ -88,7 +88,7 @@ export function Ranking() {
                 Dominant Species
                 <img 
                 src= "https://d1rw89lz12ur5s.cloudfront.net/photo/theendgames/file/12ba69d22d6042a4bc57762326789c50/dominant_species_box.jpg"
-                width="65" height="100">
+                width="90" height="125">
                 </img>
                 <select id = "dominant">
                 	<option> 5 </option>
@@ -101,7 +101,7 @@ export function Ranking() {
                 Gaia Project
                 <img 
                 src= "https://images-na.ssl-images-amazon.com/images/I/91k6yNbc8RL._AC_SL1500_.jpg"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "gproj">
                 	<option> 5 </option>
@@ -114,7 +114,7 @@ export function Ranking() {
                 The King is Dead
                 <img 
                 src= "https://images-na.ssl-images-amazon.com/images/I/81hlpdu1c-L._AC_UL600_SR600,600_.jpg"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "king">
                 	<option> 5 </option>
@@ -127,7 +127,7 @@ export function Ranking() {
                 Inis
                 <img 
                 src= "https://images-na.ssl-images-amazon.com/images/I/51G5SSMC55L._AC_SX425_.jpg"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "inis">
                 	<option> 5 </option>
@@ -140,7 +140,7 @@ export function Ranking() {
                 Cosmic Encounter
                 <img 
                 src= "https://images-na.ssl-images-amazon.com/images/I/71SEltFgKqL._AC_SL1088_.jpg"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "cosmic">
                 	<option> 5 </option>
@@ -153,7 +153,7 @@ export function Ranking() {
                 War of The Ring
                 <img
                 src="https://images-na.ssl-images-amazon.com/images/I/A10eLnKuQFL._AC_SL1500_.jpg"
-                width="100" height="100">
+                width="125" height="125">
                 </img>
                 <select id = "ring">
                 	<option> 5 </option>
@@ -165,8 +165,8 @@ export function Ranking() {
                 </select> <br /><br />
                 Neuroshima Hex
                 <img
-                src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                src= "https://images-na.ssl-images-amazon.com/images/I/A18zHeHrFzL._AC_SL1500_.jpg"
+                width="125" height="125">
                 </img>
                 <select id = "neuro">
                 	<option> 5 </option>
@@ -178,8 +178,8 @@ export function Ranking() {
                 </select> <br /><br />
                 Scythe
                 <img
-                src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                src= "https://images-na.ssl-images-amazon.com/images/I/918TkODhXPL._AC_SL1500_.jpg"
+                width="125" height="125">
                 </img>
                 <select id = "scythe">
                 	<option> 5 </option>
@@ -191,8 +191,8 @@ export function Ranking() {
                 </select> <br /><br />
                 Raiders of The North Sea
                 <img
-                src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                src= "https://i5.walmartimages.com/asr/5809ab79-0a66-4ad2-81fe-6ebe815dbf09_1.01645ba934ed99c02d8b435b296fc85e.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff"
+                width="125" height="125">
                 </img>
                 <select id = "raiders">
                 	<option> 5 </option>
@@ -204,8 +204,8 @@ export function Ranking() {
                 </select> <br /><br />
                 Above and Below
                 <img
-                src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                src= "https://images-na.ssl-images-amazon.com/images/I/911ccd3L5ML._AC_SL1500_.jpg"
+                width="125" height="125">
                 </img>
                 <select id = "above">
                 	<option> 5 </option>
@@ -215,10 +215,10 @@ export function Ranking() {
                 	<option> 1 </option>
                 	<option selected> 0 </option>
                 </select> <br /><br />
-                Dune Imperium
+                Dune: Imperium
                 <img
-                src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                src= "https://images-na.ssl-images-amazon.com/images/I/61bRCM-ft1L._AC_SL1000_.jpg"
+                width="150" height="150">
                 </img>
                 <select id = "dune">
                 	<option> 5 </option>
@@ -230,8 +230,8 @@ export function Ranking() {
                 </select> <br /><br />
                 Clank (Fantasy)
                 <img
-                src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                src= "https://images-na.ssl-images-amazon.com/images/I/818A2b0tXtL._AC_SY450_.jpg"
+                width="125" height="125">
                 </img>
                 <select id = "clankfan">
                 	<option> 5 </option>
@@ -243,8 +243,8 @@ export function Ranking() {
                 </select> <br /><br />
                 Clank (Sci-Fi)
                 <img
-                src= "https://images-na.ssl-images-amazon.com/images/I/81crhhZd63L._AC_SY355_.jpg"
-                width="100" height="100">
+                src= "https://cdn.shopify.com/s/files/1/2952/5332/products/859930005940_512x512.jpg?v=1591820843"
+                width="150" height="150">
                 </img>
                 <select id = "clankspace">
                 	<option> 5 </option>
